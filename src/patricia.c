5c2ad7741cca6b572a6ec96f377f600d22e22127
Christopher Wood <christopherwood07@gmail.com>
2016-10-11 11:45:48 -0700

    Patricia insertion split

--- src/patricia.c
+++ src/patricia.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
+#include <stdio.h>
 
+#include <parc/algol/parc_Memory.h>
 #include "patricia.h"
 
 struct _patricia_node;
-typedef struct {
+typedef struct _patricia_node {
     PARCBuffer *label;
+    void *value;
     bool isLeaf;
 
     int numEdges;
@@ -12,7 +15,7 @@ typedef struct {
 } _PatriciaNode;
 
 _PatriciaNode *
-_patriciaNode_CreateLeaf(PARCBuffer *label)
+_patriciaNode_CreateLeaf(PARCBuffer *label, void *value)
 {
     _PatriciaNode *node = (_PatriciaNode *) malloc(sizeof(_PatriciaNode));
     if (node != NULL) {
@@ -20,8 +23,23 @@ _patriciaNode_CreateLeaf(PARCBuffer *label)
         node->isLeaf = true;
         node->numEdges = 0;
         node->edges = NULL;
+        node->value = value;
     }
     return node;
+}
+
+_PatriciaNode *
+_patriciaNode_Copy(_PatriciaNode *other)
+{
+    _PatriciaNode *copy = (_PatriciaNode *) malloc(sizeof(_PatriciaNode));
+    if (copy != NULL) {
+        copy->label = parcBuffer_Acquire(other->label);
+        copy->isLeaf = other->isLeaf;
+        copy->numEdges = other->numEdges;
+        copy->edges = other->edges;
+        copy->value = other->value;
+    }
+    return copy;
 }
 
 void
@@ -29,11 +47,28 @@ _patriciaNode_AddEdge(_PatriciaNode *node, _PatriciaNode *edge)
 {
     node->numEdges++;
     if (node->edges == NULL) { 
-        node->edges = (_PatriciaNode **) malloc(sizeof(_PatriciaNode *) * mode->numEdges);
+        node->edges = (_PatriciaNode **) malloc(sizeof(_PatriciaNode *) * node->numEdges);
     } else {
-        node->edges = (_PatriciaNode **) realloc(node->edges, sizeof(_PatriciaNode *) * mode->numEdges);
+        node->edges = (_PatriciaNode **) realloc(node->edges, sizeof(_PatriciaNode *) * node->numEdges);
     }
-    node->edges[node->numEdges - 1] = edge;   
+    node->edges[node->numEdges - 1] = edge;
+    node->isLeaf = false;
+}
+
+static void
+_patriciaNode_Display(_PatriciaNode *node, int indentation)
+{
+    printf("|");
+    for (int i = 0; i < indentation; i++) {
+        printf("-");
+    }
+    char *label = parcBuffer_ToString(node->label);
+    printf("> %s\n", label);
+    parcMemory_Deallocate(&label);
+
+    for (int i = 0; i < node->numEdges; i++) {
+        _patriciaNode_Display(node->edges[i], indentation + 2);
+    };
 }
 
 struct patricia {
@@ -45,7 +80,9 @@ patricia_Create()
 {
     Patricia *patricia = (Patricia *) malloc(sizeof(Patricia));
     if (patricia != NULL) {
-        patricia->head = NULL;
+        PARCBuffer *empty = parcBuffer_Allocate(0);
+        patricia->head = _patriciaNode_CreateLeaf(empty, NULL);
+        parcBuffer_Release(&empty);
     }
     return patricia;
 }
@@ -55,53 +92,13 @@ patricia_Destroy(Patricia **patriciaP)
 {
     Patricia *patricia = *patriciaP;
     free(patricia);
-    *patricia = NULL;
+    *patriciaP = NULL;
 }
 
-void 
-patricia_Insert(Patricia *trie, PARCBuffer *key, void *item)
+static void
+_patricia_Display(Patricia *trie)
 {
-    if (trie->head == NULL) {
-        _PatriciaNode *head = _patriciaNode_CreateLeaf(key);
-        trie->head = head;
-        return;
-    }
-
-    int elementsFound = 0;
-    size_t labelLength = parcBuffer_Remaining(key);
-    _PatriciaNode *prev = NULL;
-    _PatriciaNode *current = trie->head;
-
-    // Search until we can make no further progress.
-    PARCBuffer *prefix = parcBuffer_Copy(key);
-    while (current != NULL && !current->isLeaf && elementsFound < labelLength) {
-        for (int i = 0; i < current->numEdges; i++) {
-            _PatriciaNode *next = current->edges[i];
-
-            if (_isPrefix(prefix, next->label)) {
-                prev = current;
-                current = next;
-                size_t nextLabelLength = parcBuffer_Remaining(next->label);
-                elementsFound += nextLabelLength; 
-                prefix = parcBuffer_Slice(prefix);
-                parcBuffer_SetPosition(prefix, parcBuffer_Position(prefix) + nextLabelLength);
-            } else {
-                current = NULL;
-            }
-        }
-    }
- 
-    // Handle the insertion into the trie
-    if (current == NULL && prev == NULL) {
-        // 1. Add new edge at the root
-        _PatriciaNode *newEdge = _patriciaNode_CreateLeaf(key);
-        _patriciaNode_AddEdge(trie->head, newEdge);
-    } else if (current == NULL) { // or we could split...
-        _PatriciaNode *newEdge = _patriciaNode_CreateLeaf(prefix);
-        _patriciaNode_AddEdge(prev, newEdge);
-    } else {
-        // XXX: redundant case
-    }
+    _patriciaNode_Display(trie->head, 0);
 }
 
 static bool
@@ -120,6 +117,116 @@ _isPrefix(PARCBuffer *x, PARCBuffer *y)
     return true;
 }
 
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+static int
+_sharedPrefix(PARCBuffer *x, PARCBuffer *y) 
+{
+    int minComponents = MIN(parcBuffer_Remaining(x), parcBuffer_Remaining(y));   
+
+    int shared = 0;
+    for (size_t i = 0; i < minComponents; i++) {
+        if (parcBuffer_GetAtIndex(x, parcBuffer_Position(x) + i) != parcBuffer_GetAtIndex(y, parcBuffer_Position(y) + i)) {
+            return shared;
+        }
+        shared++;
+    }
+
+    return shared;
+}
+
+void 
+patricia_Insert(Patricia *trie, PARCBuffer *key, void *item)
+{
+    /*if (trie->head == NULL) {
+        _PatriciaNode *head = _patriciaNode_CreateLeaf(key, item);
+        trie->head = head;
+        return;
+    }*/
+
+    int elementsFound = 0;
+    size_t labelLength = parcBuffer_Remaining(key);
+    _PatriciaNode *prev = NULL;
+    int prevEdgeIndex = 0;
+    _PatriciaNode *current = trie->head;
+    if (current->isLeaf) {
+        current = NULL;   
+    }
+
+    printf("BEFORE\n");
+    _patricia_Display(trie);
+
+    // Search until we can make no further progress.
+    PARCBuffer *prefix = parcBuffer_Copy(key);
+    while (current != NULL && !current->isLeaf && elementsFound < labelLength) {
+        _PatriciaNode *curr = current;
+        current = NULL;
+        for (int i = 0; i < curr->numEdges; i++) {
+            _PatriciaNode *next = curr->edges[i];
+
+            int prefixCount = parcBuffer_Remaining(prefix);
+            int sharedCount = _sharedPrefix(prefix, next->label);
+
+            printf("Checking prefix: %s %s\n", parcBuffer_ToString(prefix), parcBuffer_ToString(next->label));
+
+            if (prefixCount == sharedCount) { // jump to the next level
+                prevEdgeIndex = i;
+                prev = curr;
+                current = next;
+                size_t nextLabelLength = parcBuffer_Remaining(next->label);
+                elementsFound += nextLabelLength; 
+                prefix = parcBuffer_Slice(prefix);
+                parcBuffer_SetPosition(prefix, parcBuffer_Position(prefix) + nextLabelLength);
+                break;
+            } else if (sharedCount > 0) { // split up the node
+                // abc | abd -> ab, c, d
+                PARCBuffer *sharedPrefix = parcBuffer_Slice(next->label); // "ab"
+                PARCBuffer *leftPrefix = parcBuffer_Copy(sharedPrefix); // "c"
+                parcBuffer_SetLimit(sharedPrefix, sharedCount);
+                parcBuffer_SetPosition(leftPrefix, parcBuffer_Position(leftPrefix) + sharedCount);
+                PARCBuffer *rightPrefix = parcBuffer_Slice(prefix); // "d"
+                parcBuffer_SetPosition(rightPrefix, parcBuffer_Position(rightPrefix) + sharedCount);
+                
+                _PatriciaNode *splitRight = _patriciaNode_CreateLeaf(rightPrefix, item);
+                _PatriciaNode *splitLeft = _patriciaNode_CreateLeaf(sharedPrefix, NULL); // no item goes here
+
+                printf("%s %s %s\n", parcBuffer_ToString(sharedPrefix), parcBuffer_ToString(leftPrefix), parcBuffer_ToString(rightPrefix));
+                
+                curr->edges[i] = splitLeft;
+                // Add the left node to the split
+                next->label = leftPrefix;
+                _patriciaNode_AddEdge(splitLeft, next);
+
+                // Add the right node to the split    
+                _patriciaNode_AddEdge(splitLeft, splitRight);
+
+                prev = splitLeft;
+                current = splitRight;
+                parcBuffer_SetPosition(prefix, parcBuffer_Position(prefix) + parcBuffer_Remaining(prefix));
+
+                printf("AFTER\n");
+                _patricia_Display(trie);
+
+                return;
+            } else { // nothing in common
+                current = NULL; // does nothing.
+            }
+        }
+    }
+ 
+    // Handle the insertion into the trie
+    if (current == NULL) { 
+        _PatriciaNode *newEdge = _patriciaNode_CreateLeaf(prefix, item);
+        _PatriciaNode *target = prev == NULL ? trie->head : prev;
+        printf("Adding %s to HEAD\n", parcBuffer_ToString(prefix));
+        _patriciaNode_AddEdge(target, newEdge);
+    } else if (current->isLeaf) {
+        _PatriciaNode *newEdge = _patriciaNode_CreateLeaf(prefix, item);
+        _patriciaNode_AddEdge(current, newEdge);
+        printf("Adding %s to trie\n", parcBuffer_ToString(prefix));
+    }
+}
+
 void *
 patricia_Get(Patricia *trie, PARCBuffer *key)
 {
@@ -127,30 +234,40 @@ patricia_Get(Patricia *trie, PARCBuffer *key)
     size_t labelLength = parcBuffer_Remaining(key);
     _PatriciaNode *current = trie->head;
 
+    printf("GETTING %s\n", parcBuffer_ToString(key));
+    _patricia_Display(trie);
+
     PARCBuffer *prefix = parcBuffer_Copy(key);
     while (current != NULL && !current->isLeaf && elementsFound < labelLength) {
-        for (int i = 0; i < current->numEdges; i++) {
-            _PatriciaNode *next = current->edges[i];
+        _PatriciaNode *curr = current;
+        current = NULL;
+        printf("number of edges = %d\n", curr->numEdges);
+        for (int i = 0; i < curr->numEdges; i++) {
+            _PatriciaNode *next = curr->edges[i];
+            
+            int sharedCount = _sharedPrefix(prefix, next->label);
 
-            if (_isPrefix(prefix, next->label)) {
+            printf("Checking prefix: %s %s\n", parcBuffer_ToString(prefix), parcBuffer_ToString(next->label));
+            printf("break %d\n", sharedCount);
+
+            if (sharedCount > 0) { // jump to the next level
                 current = next;
                 size_t nextLabelLength = parcBuffer_Remaining(next->label);
                 elementsFound += nextLabelLength; 
-                prefix = parcBuffer_Slice(prefix);
                 parcBuffer_SetPosition(prefix, parcBuffer_Position(prefix) + nextLabelLength);
-            } else {
-                current = NULL;
-            }
+                break;
+            } 
         }
     }
     
     if (current == NULL) {
-        return false;
+        printf("here\n");
+        return NULL;
     } else if (current->isLeaf && elementsFound == labelLength) {
-        return true;
+        return current->value;
     } else {
-        return false;
+        return NULL;
     }
 }
 
-#endif // patricia_h_
+
---
+++ src/test/test_patricia.c
@@ -0,0 +1,148 @@
+#include "../patricia.h"
+
+#include <stdio.h>
+
+#include <LongBow/testing.h>
+#include <LongBow/debugging.h>
+
+#include <parc/algol/parc_Memory.h>
+#include <parc/algol/parc_SafeMemory.h>
+
+#include <parc/testing/parc_MemoryTesting.h>
+
+LONGBOW_TEST_RUNNER(patricia)
+{
+    LONGBOW_RUN_TEST_FIXTURE(Core);
+}
+
+// The Test Runner calls this function once before any Test Fixtures are run.
+LONGBOW_TEST_RUNNER_SETUP(patricia)
+{
+    return LONGBOW_STATUS_SUCCEEDED;
+}
+
+// The Test Runner calls this function once after all the Test Fixtures are run.
+LONGBOW_TEST_RUNNER_TEARDOWN(patricia)
+{
+    return LONGBOW_STATUS_SUCCEEDED;
+}
+
+LONGBOW_TEST_FIXTURE(Core)
+{
+    LONGBOW_RUN_TEST_CASE(Core, patricia_Create);
+    LONGBOW_RUN_TEST_CASE(Core, patricia_Insert_Empty);
+    LONGBOW_RUN_TEST_CASE(Core, patricia_Insert_Sibling);
+    LONGBOW_RUN_TEST_CASE(Core, patricia_Insert_Split);
+}
+
+LONGBOW_TEST_FIXTURE_SETUP(Core)
+{
+    parcMemory_SetInterface(&PARCSafeMemoryAsPARCMemory);
+    return LONGBOW_STATUS_SUCCEEDED;
+}
+
+LONGBOW_TEST_FIXTURE_TEARDOWN(Core)
+{
+    //if (!parcMemoryTesting_ExpectedOutstanding(0, "%s mismanaged memory.", longBowTestCase_GetFullName(testCase))) {
+    //    return LONGBOW_STATUS_MEMORYLEAK;
+    //}
+
+    return LONGBOW_STATUS_SUCCEEDED;
+}
+
+LONGBOW_TEST_CASE(Core, patricia_Create)
+{
+    Patricia *trie = patricia_Create();
+    assertNotNull(trie, "Expected a non-NULL trie to be created");
+    patricia_Destroy(&trie);
+    assertNull(trie, "Expected a NULL trie after map_Destroy");
+}
+
+LONGBOW_TEST_CASE(Core, patricia_Insert_Empty)
+{
+    Patricia *trie = patricia_Create();
+    assertNotNull(trie, "Expected a non-NULL trie to be created");
+
+    PARCBuffer *key = parcBuffer_AllocateCString("abc");
+    PARCBuffer *value = parcBuffer_AllocateCString("hello");
+   
+    patricia_Insert(trie, key, (void *) value);
+    PARCBuffer *actual = (PARCBuffer *) patricia_Get(trie, key);
+
+    assertNotNull(actual, "Expected to get something back");
+    assertTrue(parcBuffer_Equals(actual, value), "Expected to get the same value back");
+    
+    parcBuffer_Release(&key);
+    parcBuffer_Release(&value);
+    patricia_Destroy(&trie);
+}
+
+LONGBOW_TEST_CASE(Core, patricia_Insert_Sibling)
+{
+    printf("\n\n SIBLING\n");
+    Patricia *trie = patricia_Create();
+    assertNotNull(trie, "Expected a non-NULL trie to be created");
+
+    PARCBuffer *key1 = parcBuffer_AllocateCString("abc");
+    PARCBuffer *value1 = parcBuffer_AllocateCString("hello1");
+    PARCBuffer *key2 = parcBuffer_AllocateCString("def");
+    PARCBuffer *value2 = parcBuffer_AllocateCString("value2");
+   
+    patricia_Insert(trie, key1, (void *) value1);
+    PARCBuffer *actual = (PARCBuffer *) patricia_Get(trie, key1);
+
+    assertNotNull(actual, "Expected to get something back");
+    assertTrue(parcBuffer_Equals(actual, value1), "Expected to get the same value back");
+
+    patricia_Insert(trie, key2, (void *) value2);
+    actual = (PARCBuffer *) patricia_Get(trie, key2);
+
+    assertNotNull(actual, "Expected to get something back");
+    assertTrue(parcBuffer_Equals(actual, value2), "Expected to get the same value back");
+    
+    parcBuffer_Release(&key1);
+    parcBuffer_Release(&value1);
+    parcBuffer_Release(&key2);
+    parcBuffer_Release(&value2);
+    patricia_Destroy(&trie);
+}
+
+LONGBOW_TEST_CASE(Core, patricia_Insert_Split)
+{
+
+    printf("\n\n SPLIT\n");
+    Patricia *trie = patricia_Create();
+    assertNotNull(trie, "Expected a non-NULL trie to be created");
+
+    PARCBuffer *key1 = parcBuffer_AllocateCString("abc");
+    PARCBuffer *value1 = parcBuffer_AllocateCString("hello1");
+    PARCBuffer *key2 = parcBuffer_AllocateCString("abd");
+    PARCBuffer *value2 = parcBuffer_AllocateCString("value2");
+   
+    patricia_Insert(trie, key1, (void *) value1);
+    PARCBuffer *actual = (PARCBuffer *) patricia_Get(trie, key1);
+
+    assertNotNull(actual, "Expected to get something back");
+    assertTrue(parcBuffer_Equals(actual, value1), "Expected to get the same value before the split");
+
+    patricia_Insert(trie, key2, (void *) value2);
+    actual = (PARCBuffer *) patricia_Get(trie, key2);
+
+    assertNotNull(actual, "Expected to get something back");
+    assertTrue(parcBuffer_Equals(actual, value2), "Expected to get the same value back after the split");
+    
+    parcBuffer_Release(&key1);
+    parcBuffer_Release(&value1);
+    parcBuffer_Release(&key2);
+    parcBuffer_Release(&value2);
+    patricia_Destroy(&trie);
+}
+
+int
+main(int argc, char *argv[argc])
+{
+    LongBowRunner *testRunner = LONGBOW_TEST_RUNNER_CREATE(patricia);
+    int exitStatus = longBowMain(argc, argv, testRunner, NULL);
+    longBowTestRunner_Destroy(&testRunner);
+    exit(exitStatus);
+}
