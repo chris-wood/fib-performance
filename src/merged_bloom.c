46a942a4a3a6aadb2baa1db449299bcea4a14d4a
Christopher Wood <christopherwood07@gmail.com>
2016-12-01 20:45:34 -0800

    Merged stuff

--- src/merged_bloom.c
+++ src/merged_bloom.c
@@ -3,12 +3,29 @@
 //
 
 #include "merged_bloom.h"
+#include "bloom.h"
 
 struct merged_bloom_filter {
     int N;
     int m;
     int k;
+
+    BloomFilter **filters;
 };
+
+void
+mergedBloomFilter_Destroy(MergedBloomFilter **filterP)
+{
+    MergedBloomFilter *filter = *filterP;
+
+    for (int i = 0; i < filter->N; i++) {
+        bloom_Destroy(&filter->filters[i]);
+    }
+    free(filter->filters);
+
+    free(filter);
+    *filterP = NULL;
+}
 
 MergedBloomFilter *
 mergedBloomFilter_Create(int N, int m, int k) // N and m determine the matrix dimensions
@@ -19,21 +36,18 @@ mergedBloomFilter_Create(int N, int m, int k) // N and m determine the matrix di
         filter->m = m;
         filter->k = k;
 
-        // XXX
+        filter->filters = (BloomFilter **) malloc(sizeof(BloomFilter *) * N);
+        for (int i = 0; i < N; i++) {
+            filter->filters[i] = bloom_Create(m, k);
+        }
     }
     return filter;
 }
 
 void
-mergedBloomFilter_Destroy(MergedBloomFilter **bfP)
-{
-
-}
-
-void
 mergedBloomFilter_Add(MergedBloomFilter *filter, Name *name, PARCBitVector *vector)
 {
-
+    
 }
 
 bool
