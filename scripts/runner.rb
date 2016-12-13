ea88f67ab8ebd28dc376c9c0285614385c32bf9e
Christopher Wood <christopherwood07@gmail.com>
2016-12-13 07:09:57 -0800

    Runner updates

--- scripts/runner.rb
+++ scripts/runner.rb
@@ -8,16 +8,20 @@ fib_perf = ARGV[0]
 load_file = ARGV[1]
 test_file = ARGV[2]
 
-algorithms = ["naive", "cisco"] #, "caesar", "caesar-filter"]
+table_algorithms = ["naive", "cisco"]
+filter_algorithms = [] #"caesar", "caesar-filter", "merged-bf"]
 widths = [4, 8, 12, 16, 20, 24, 28, 32]
+numfilters = [2, 3, 4, 5, 6]
 
-algorithms.each {|alg| 
+table_algorithms.each {|alg| 
     fname = "#{alg}_out.txt"
 
+    # Run without hashing
     output = `#{fib_perf} -l #{load_file} -t #{test_file} -a #{alg}`
     puts output
     File.open(fname, 'a') { |file| file.write(output) }
  
+    # Run with hashing
     widths.each {|width|   
         output_hashed = `#{fib_perf} -l #{load_file} -t #{test_file} -a #{alg} -d #{width}`
         puts output_hashed 
@@ -25,4 +29,20 @@ algorithms.each {|alg|
     }
 
     #exit
+}
+
+filters_algorithms.each {|alg| 
+    numfilters.each{|k| 
+        fname = "#{alg}_#{k}_out.txt"
+
+        output = `#{fib_perf} -l #{load_file} -t #{test_file} -a #{alg} -f #{k}`
+        puts output
+        File.open(fname, 'a') { |file| file.write(output) }
+
+        widths.each {|width|   
+            output_hashed = `#{fib_perf} -l #{load_file} -t #{test_file} -a #{alg} -d #{width} -f #{k}`
+            puts output_hashed 
+            File.open(fname, 'a') { |file| file.write(output_hashed) }
+        }
+    }
 }
